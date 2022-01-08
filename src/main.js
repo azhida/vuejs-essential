7f450b7eb14370f3caa6caeb4a5eb5e803f9ab19
azhida <334064034@qq.com>
2022-01-08 14:19:03 +0800

    添加修改提示

--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from './App'
 import router from './router'
-import store from './store'
-import VueSweetalert2 from './plugins/vue-sweetalert2'
 import './directives'
 import './components'
+import store from './store'
+import VueSweetalert2 from './plugins/vue-sweetalert2'
+// 引入插件
+import Message from './plugins/message'
 
 Vue.use(VueSweetalert2)
+// 使用插件
+Vue.use(Message)
 
 Vue.config.productionTip = false
 
