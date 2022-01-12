271c2d49a2bfcb132ab88a69457d2cb3bcc4ca6a
azhida <334064034@qq.com>
2022-01-12 17:15:21 +0800

    打包

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,9 @@
 const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/vuejs-essential/dist/'
-    : '/',
+  // publicPath: process.env.NODE_ENV === 'production'
+  //   ? '/vuejs-essential/dist/'
+  //   : '/',
 
   // npm run build 打包输出的目录，默认 dist 目录， 打包到 docs 是为了在 github pages 上使用
   // outputDir: 'docs',
