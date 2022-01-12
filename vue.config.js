383c9c08f06aaacb732df9f9ee1797f8729bc57d
azhida <334064034@qq.com>
2022-01-12 16:48:49 +0800

    引入 插件 copy-webpack-plugin ，将 src/CNAME 打包到dist

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
+const CopyPlugin = require('copy-webpack-plugin');
+
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/vuejs-essential/dist/'
@@ -5,4 +7,13 @@ module.exports = {
 
   // npm run build 打包输出的目录，默认 dist 目录， 打包到 docs 是为了在 github pages 上使用
   // outputDir: 'docs',
+
+  configureWebpack: config => {
+    config.plugins = [
+      ...config.plugins,
+      new CopyPlugin([
+        { from: 'src/CNAME', to: '' }
+      ]),
+    ]
+  },
 }
